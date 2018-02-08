c0e22f820d86eaac42afdcbfee7cb36e354729c0
amankumar367 <kumaraman367@gmail.com>
2018-02-08 14:31:02 +0530

    8th Feb 2:03PM

--- app/src/main/java/com/example/aman/hospitalappointy/MainActivity.java
+++ app/src/main/java/com/example/aman/hospitalappointy/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.aman.hospitalappointy;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 
@@ -11,10 +12,17 @@ public class MainActivity extends AppCompatActivity {
 
     private Button mLogin;
 
+    private Toolbar mToolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mToolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("Hospital Appointy");
+
 
         mLogin = (Button) findViewById(R.id.main_login_btn);
         mLogin.setOnClickListener(new View.OnClickListener() {
