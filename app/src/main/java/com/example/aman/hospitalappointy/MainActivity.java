aadb2aea9047217eca79aa25d3eae8fa1e2dd0d4
amankumar367 <kumaraman367@gmail.com>
2018-02-08 06:59:27 +0530

    8th Feb Files

--- app/src/main/java/com/example/aman/hospitalappointy/MainActivity.java
+++ app/src/main/java/com/example/aman/hospitalappointy/MainActivity.java
@@ -10,30 +10,19 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     private Button mLogin;
-    private Button mRegister;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         mLogin = (Button) findViewById(R.id.main_login_btn);
-        mRegister = (Button) findViewById(R.id.main_reg_btn);
-        //this is our project
         mLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 Intent login_Intent = new Intent(MainActivity.this, LoginActivity.class);
                 startActivity(login_Intent);
-            }
-        });
-
-        mRegister.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                Intent registration_Intent = new Intent(MainActivity.this, Patient_RegistrationActivity.class);
-                startActivity(registration_Intent);
             }
         });
     }
--- app/src/main/java/com/example/aman/hospitalappointy/Patient_RegistrationActivity.java
+++ app/src/main/java/com/example/aman/hospitalappointy/Patient_RegistrationActivity.java
@@ -1,17 +1,23 @@
 package com.example.aman.hospitalappointy;
 
+import android.app.ProgressDialog;
 import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+
+import org.w3c.dom.Text;
 
 public class Patient_RegistrationActivity extends AppCompatActivity {
 
@@ -21,12 +27,22 @@ public class Patient_RegistrationActivity extends AppCompatActivity {
 
     //Firebase Auth
     private FirebaseAuth mAuth;
+    private Toolbar mToolbar;
+    private ProgressDialog mRegProgress;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient__registration);
 
         mAuth = FirebaseAuth.getInstance();
+
+        // Toolbar
+        mToolbar = (Toolbar) findViewById(R.id.register_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("Registration");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mRegProgress = new ProgressDialog(this);
 
         mEmail = (TextInputLayout) findViewById(R.id.reg_email_layout);
         mPassword = (TextInputLayout) findViewById(R.id.reg_password_layout);
@@ -39,16 +55,45 @@ public class Patient_RegistrationActivity extends AppCompatActivity {
                 String email = mEmail.getEditText().getText().toString();
                 String password = mPassword.getEditText().getText().toString();
 
-                mAuth.createUserWithEmailAndPassword(email,password)
-                        .addOnCompleteListener(Patient_RegistrationActivity.this, new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(!TextUtils.isEmpty(email) && !TextUtils.isEmpty(password)){
 
-                                Toast.makeText(Patient_RegistrationActivity.this,"Successfully Account Created",Toast.LENGTH_LONG).show();
-                            }
-                        });
+                    mRegProgress.setTitle("Creating Account");
+                    mRegProgress.setMessage("Please Wait! We are Processing");
+                    mRegProgress.setCanceledOnTouchOutside(false);
+                    mRegProgress.show();
+
+                    createAccount(email,password);
+
+                }
+                else{
+
+                    Toast.makeText(Patient_RegistrationActivity.this,"Please enter email & password",Toast.LENGTH_LONG).show();
+
+                }
 
             }
         });
+    }
+
+    private void createAccount(String email, String password) {
+
+        mAuth.createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener(Patient_RegistrationActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if(task.isSuccessful()){
+
+                            mRegProgress.dismiss();
+                            Toast.makeText(Patient_RegistrationActivity.this,"Successfully Account Created",Toast.LENGTH_LONG).show();
+                        }
+                        else {
+
+                            mRegProgress.dismiss();
+                            Toast.makeText(Patient_RegistrationActivity.this,"Creating Account Failed",Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+
     }
 }
