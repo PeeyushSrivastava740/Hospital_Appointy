6e38bb7f624ac4fdc2423fc317d9d86079828e94
DELL <divyagajbhiye98@gmail.com>
2018-02-14 22:05:48 +0000

    First Commit

--- app/src/main/java/com/example/aman/hospitalappointy/SplashScreenActivity.java
+++ app/src/main/java/com/example/aman/hospitalappointy/SplashScreenActivity.java
@@ -1,13 +1,53 @@
 package com.example.aman.hospitalappointy;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SplashScreenActivity extends AppCompatActivity {
+    TextView txt;
+    ImageView logo;
+    Animation uptodown, downtoup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        logo = (ImageView) findViewById(R.id.logoid);
+        txt = (TextView) findViewById(R.id.textid);
+        uptodown = AnimationUtils.loadAnimation(this,R.anim.uptodown);
+        downtoup = AnimationUtils.loadAnimation(this,R.anim.downtoup);
+
+        logo.setAnimation(uptodown);
+        txt.setAnimation(downtoup);
+
+
+        Thread thread = new Thread(){
+            @Override
+            public void run() {
+                try{
+                    sleep(5000);
+                }
+                catch(Exception e){
+                    e.printStackTrace();
+                }
+                finally
+                {
+                    Intent i= new Intent(SplashScreenActivity.this,searchActivity.class);
+                    startActivity(i);
+                }
+            }
+        };
+        thread.start();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
     }
 }
