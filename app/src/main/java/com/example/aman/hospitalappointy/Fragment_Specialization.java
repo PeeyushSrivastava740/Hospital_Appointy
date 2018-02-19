68621a0e8b281d2abc46e88a04ec0f9ead9ef70f
amankumar367 <kumaraman367@gmail.com>
2018-02-20 05:05:04 +0530

    14th Feb 04:25 PM

--- app/src/main/java/com/example/aman/hospitalappointy/Fragment_Specialization.java
+++ app/src/main/java/com/example/aman/hospitalappointy/Fragment_Specialization.java
@@ -1,6 +1,7 @@
 package com.example.aman.hospitalappointy;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -8,7 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.widget.Button;
 
 /**
  * Created by Aman on 14-Feb-18.
@@ -16,6 +17,7 @@ import android.widget.ListView;
 
 public class Fragment_Specialization extends Fragment {
 
+    private Button mDocProfile;
 
     public Fragment_Specialization(){
         //Required Empty public constructor otherwise app will crash
@@ -25,7 +27,18 @@ public class Fragment_Specialization extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_specialization,container,false);
+        final View rootView = inflater.inflate(R.layout.fragment_specialization,container,false);
+
+        mDocProfile = (Button) rootView.findViewById(R.id.doc_profile);
+        mDocProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent special_Intent = new Intent(rootView.getContext(), Patient_DoctorProfileActivity.class);
+                startActivity(special_Intent);
+            }
+        });
+
+        return rootView;
     }
 
 
